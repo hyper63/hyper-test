a827e0f39acbea0ff5004a4761acc090c443f35a
Tom Wilson <twilson63@gmail.com>
2021-08-30 17:52:03 +0000

    handle db not found (#4)

--- hyper.js
+++ hyper.js
@@ -2,8 +2,7 @@ import { default as hyper } from "https://x.nest.land/hyper@1.4.3/mod.js";
 import { default as app } from "https://x.nest.land/hyper-app-opine@1.2.2/mod.js";
 
 // adapters
-import { default as dndb } from "https://x.nest.land/hyper-adapter-dndb@0.0.11/mod.js";
-
+import { default as dndb } from "https://x.nest.land/hyper-adapter-dndb@0.0.12/mod.js";
 // import { default as memory } from "https://x.nest.land/hyper-adapter-memory@1.2.6/mod.js";
 // import { default as fs } from "https://x.nest.land/hyper-adapter-fs@1.0.8/mod.js";
 // import { default as minisearch } from "https://x.nest.land/hyper-adapter-minisearch@1.0.11/mod.js";
